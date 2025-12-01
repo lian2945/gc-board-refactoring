f92d1919458d2bd3ad74ba8493d20cc5c38af84a
Lian08 <lian080408@gmail.com>
2025-12-01 13:34:36 +0900

    Project Setting

---
+++ src/main/java/gcboard/gcboardrefactoring/GcBoardRefactoringApplication.java
@@ -0,0 +1,13 @@
+package gcboard.gcboardrefactoring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GcBoardRefactoringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GcBoardRefactoringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/gcboard/gcboardrefactoring/GcBoardRefactoringApplicationTests.java
@@ -0,0 +1,13 @@
+package gcboard.gcboardrefactoring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GcBoardRefactoringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
